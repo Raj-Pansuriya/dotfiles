647fa50f2f794c257bc759abb0b56cc22767aa48
raj <rajpansuriya40@gmail.com>
2021-10-02 11:51:15 +0530

    Update the floating window rules by qtile

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -29,8 +29,9 @@ import os
 import re
 import socket
 import subprocess
-from libqtile import qtile
-from libqtile.config import Drag, Key, Screen, Group, Drag, Click, Rule, KeyChord
+from typing import List  # noqa: F401
+from libqtile import qtile, layout, bar, widget, hook
+from libqtile.config import Click, Drag, Group, Key, Match, Screen, Rule, Group, KeyChord
 from libqtile.command import lazy
 from libqtile.lazy import lazy
 from libqtile import layout, bar, widget, hook
@@ -58,8 +59,6 @@ def window_to_next_group(qtile):
         qtile.currentWindow.togroup(qtile.groups[i + 1].name)
 
 keys = [
-
-### SUPER KEY
 
 # Qtile Functions
     Key([mod, "shift"], "r",
@@ -263,7 +262,7 @@ layouts = [
 ]
 
 # COLORS FOR THE BAR
-
+# Theme: Dracula [DT]
 def init_colors():
     return [["#282c34", "#282c34"], # panel background
             ["#3d3f4b", "#434758"], # background for current screen tab
@@ -275,8 +274,6 @@ def init_colors():
             ["#ecbbfb", "#ecbbfb"]] # backbround for inactive screens
 
 colors = init_colors()
-
-prompt = "{0}@{1}: ".format(os.environ["USER"], socket.gethostname())
 
 
 # WIDGETS FOR THE BAR
@@ -290,6 +287,7 @@ widget_defaults = dict(
 extension_defaults = widget_defaults.copy()
 
 def init_widgets_list():
+    prompt = "{0}@{1}: ".format(os.environ["USER"], socket.gethostname())
     widgets_list = [
               widget.Sep(
                        linewidth = 0,
@@ -648,29 +646,30 @@ follow_mouse_focus = True
 bring_front_click = False
 cursor_warp = False
 floating_layout = layout.Floating(float_rules=[
-    {'wmclass': 'Arcolinux-welcome-app.py'},
-    {'wmclass': 'Arcolinux-tweak-tool.py'},
-    {'wmclass': 'Arcolinux-calamares-tool.py'},
-    {'wmclass': 'confirm'},
-    {'wmclass': 'dialog'},
-    {'wmclass': 'download'},
-    {'wmclass': 'error'},
-    {'wmclass': 'file_progress'},
-    {'wmclass': 'notification'},
-    {'wmclass': 'splash'},
-    {'wmclass': 'toolbar'},
-    {'wmclass': 'confirmreset'},
-    {'wmclass': 'makebranch'},
-    {'wmclass': 'maketag'},
-    {'wmclass': 'Arandr'},
-    {'wmclass': 'feh'},
-    {'wmclass': 'Galculator'},
-    {'wmclass': 'arcolinux-logout'},
-    {'wmclass': 'xfce4-terminal'},
-    {'wname': 'branchdialog'},
-    {'wname': 'Open File'},
-    {'wname': 'pinentry'},
-    {'wmclass': 'ssh-askpass'},
+    # Run the utility of `xprop` to see the wm class and name of an X client.
+    *layout.Floating.default_float_rules, 
+    Match(wm_class='confirmreset'),  # gitk
+    Match(wm_class='makebranch'),  # gitk
+    Match(wm_class='maketag'),  # gitk
+    Match(wm_class='ssh-askpass'),  # ssh-askpass
+    Match(title='branchdialog'),  # gitk
+    Match(title='pinentry'),  # GPG key password entry
+    Match(wm_class='Arcolinux-welcome-app.py'),
+    Match(wm_class='Arcolinux-tweak-tool.py'),
+    Match(wm_class='Arcolinux-calamares-tool.py'),
+    Match(wm_class='confirm'),
+    Match(wm_class='dialog'),
+    Match(wm_class='download'),
+    Match(wm_class='error'),
+    Match(wm_class='file_progress'),
+    Match(wm_class='notification'),
+    Match(wm_class='splash'),
+    Match(wm_class='toolbar'),
+    Match(wm_class='Arandr'),
+    Match(wm_class='feh'),
+    Match(wm_class='Galculator'),
+    Match(wm_class='arcolinux-logout'),
+    Match(wm_class='xfce4-terminal'),
 
 ],  fullscreen_border_width = 0, border_width = 0)
 auto_fullscreen = True
